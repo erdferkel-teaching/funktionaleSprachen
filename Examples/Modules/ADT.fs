3c5cbba3e894fb1e1993b548702d6cdb07d4d14e
Harald Steinlechner <haraldsteinlechner@gmail.com>
2017-11-16 10:45:44 +0100

    added module stuff

---
+++ Examples/Modules/ADT.fs
@@ -0,0 +1,16 @@
+﻿namespace ADT
+
+type AssocList<'k,'v> = private { list : list<'k*'v> }
+
+module AssocList =
+    
+    let ofList xs = { list = xs }
+
+    let addOrUpdate (k : 'k) (v : 'v) (l : AssocList<'k, 'v>) =
+        let rec doIt k v l =
+             match l with
+                | [] -> [k,v]
+                | (k',v') :: xs -> 
+                    if k' = k then (k',v) :: xs
+                    else (k',v') :: doIt k v xs
+        { list = doIt k v l.list }
